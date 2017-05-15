10080d90ffbdea247c29ff4577de3da20796f3ff
Doug <doug.steinberg@gmail.com>
2017-05-15 12:36:56 -0400

    Finished Exercise 1

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,35 @@
+#Numbers
 puts 2
 puts 3
 puts 2 + 3
+
+
+#Strings
+puts 'Hello world'
+puts "Hello world"
+
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+#Symbols
+
+
+#Booleans
+(1 < 3) && (3 < 5) #true
+
+(1 > 1) && (2 > 2) #false
+
+(1 == 2) || (2 < 3) #true
+
+(1 != 2) || (2 < 3) #true
+
+puts (1 == 2) || (2 == 3) #false
